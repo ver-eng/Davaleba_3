7cec0abcc7a49f898ba7dcbaa4414fd426de5b76
Veriko-1 <v.patsinashvili@gmail.com>
2026-06-12 00:35:33 +0400

    Add task 2

---
+++ src/main/java/org/example/GeorgianCityGenerator.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GeorgianCityGenerator {
+    String city;
+    String region;
+
+    public GeorgianCityGenerator(String city, String region){
+
+        this.city=city;
+        this.region=region;
+
+    }
+    @Override
+    public String toString(){
+        return city+" - "+region;
+    }
+    public static void main(String[] args) {
+
+        List<GeorgianCityGenerator> cities=new ArrayList<>();
+
+        cities.add(new GeorgianCityGenerator("თბილისი", "ქართლი"));
+        cities.add(new GeorgianCityGenerator("რუსთავი", "ქვემო ქართლი"));
+        cities.add(new GeorgianCityGenerator("ბათუმი", "აჭარა"));
+        cities.add(new GeorgianCityGenerator("ქუთაისი", "იმერეთი"));
+        cities.add(new GeorgianCityGenerator("თელავი", "კახეთი"));
+
+        System.out.println(getRandomCity(cities));
+    }
+
+
+    public static String getRandomCity(List citiesad){
+        String randomCity=citiesad.get(new Random().nextInt(citiesad.size())).toString();
+        return randomCity;
+
+    }
+}
--- src/main/java/org/example/RestaurantMenu.java
+++ src/main/java/org/example/RestaurantMenu.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,20 +8,14 @@ public class RestaurantMenu {
 
     public static void main(String[] args) {
 
-//        Map<String, List<String>> menuItems=Map.of(
-//                "სტარტერი", List.of("მოცარელას ჩხირები", "ხახვის რგოლები", "ქათმის ფრთები"),
-//                "დესერტი", List.of("ბრაუნი", "ნაყინი", "პანაკოტა"),
-//                "წვნიანი", List.of("სოკოს სუპი", "ოსპის წვნიანი", "ქათმის წვნიანი"));
-
         Map<String, List<String>> menuItems=new HashMap<>();
 
         menuItems.put("სტარტერი", List.of("მოცარელას ჩხირები", "ხახვის რგოლები", "ქათმის ფრთები")) ;
         menuItems.put("დესერტი", List.of("ბრაუნი", "ნაყინი", "პანაკოტა"));
         menuItems.put("წვნიანი", List.of("სოკოს სუპი", "ოსპის წვნიანი", "ქათმის წვნიანი"));
+
         printMenuForCategory(menuItems.get("დესერტი").toString());
-
-
-
+        printMenuForCategory(menuItems.get("სტარტერი").toString());
 
 
     }
