3b9195d2b255fdfa459fe47310ee1abd856378fe
Veriko-1 <v.patsinashvili@gmail.com>
2026-06-19 00:56:43 +0400

    Add task 7

---
+++ src/main/java/org/example/ProductSorter.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSorter {
+    String title;
+    double price;
+
+    public ProductSorter(String title, double price){
+        this.title=title;
+        this.price=price;
+    }
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Price: " + price;
+    }
+    public static void main(String[] args) {
+
+        List<ProductSorter> products=new ArrayList<>();
+
+        products.add(new ProductSorter("ბალი", 10));
+        products.add(new ProductSorter("მარწყვი", 9));
+        products.add(new ProductSorter("ალუჩა", 6.5));
+        products.add(new ProductSorter("ყავა", 39));
+        products.add(new ProductSorter("პური", 0.99));
+        products.add(new ProductSorter("სდფსფსც", 12.99));
+
+
+        products.sort((a,b)->Double.compare(a.price, b.price));
+        System.out.println(products);
+
+    }
+}
